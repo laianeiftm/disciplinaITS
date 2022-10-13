74aa340b2ea467ded7d0046f22215879b54537bf
Laiane Ribeiro <Laiane Ribeiro@DESKTOP-94BMI1G>
2022-10-12 23:30:11 -0300

    Implementação de novos testes com ajustes e ajustes na classe Avaliador

--- projetoAvalidador-ModeloTeste/src/main/java/org/iftm/entities/Avaliador.java
+++ projetoAvalidador-ModeloTeste/src/main/java/org/iftm/entities/Avaliador.java
@@ -1,14 +1,50 @@
 package org.iftm.entities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Avaliador {
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+	private double menorDeTodos = Double.POSITIVE_INFINITY;
+	public double getMaiorLance() {return maiorDeTodos;}
+	public double getMenorLance() { return menorDeTodos;}
+	private List<Lance> maiores;
+	
 	public void avalia(Leilao leilao) {
-		for(Lance lance : leilao.getLances()) {
-			if(lance.getValor() > maiorDeTodos) {
+		for (Lance lance : leilao.getLances()) {
+			if (lance.getValor() > maiorDeTodos) {
 				maiorDeTodos = lance.getValor();
 			}
+			if (lance.getValor() < menorDeTodos) {
+				menorDeTodos = lance.getValor();
+			}
 		}
+		pegaOsMaioresNo(leilao);
 	}
-	public double getMaiorLance() {
-		return maiorDeTodos;
+	
+	
+	private void pegaOsMaioresNo(Leilao leilao) {
+		maiores = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+			public int compare(Lance o1, Lance o2) {
+				if (o1.getValor() < o2.getValor())
+					return 1;
+				if (o1.getValor() > o2.getValor())
+					return -1;
+				return 0;
+			}
+		});
+		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
 	}
-}
+
+	public void avalia1(Leilao leilao) {
+		// lançando a exceção
+		if(leilao.getLances().size() ==0)
+		throw new RuntimeException(
+		"Não é possível avaliar um leilão sem lances"
+		);
+		//resto similar
+	}
+	}
--- projetoAvalidador-ModeloTeste/src/test/java/org/iftm/entity/AvaliadorTest.java
+++ projetoAvalidador-ModeloTeste/src/test/java/org/iftm/entity/AvaliadorTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.iftm.entities.Avaliador;
 import org.iftm.entities.Lance;
 import org.iftm.entities.Leilao;
 import org.iftm.entities.Usuario;
 import org.junit.jupiter.api.Test;
+
 
 // criando objetos usuario, leilao, e avaliador 
 public class AvaliadorTest{
@@ -39,10 +42,93 @@ public class AvaliadorTest{
 		// comparar
 		double resultadoObtido = avaliador.getMaiorLance();
 		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
-		
+	}
+	
+	
+		@Test
+		public void testarMaiorLancesOrdemCrescente() {
 
+		// cenário: 3 lances sem ordem
+		leilao.propoe(new Lance(usuario3,250.0));
+		leilao.propoe(new Lance(usuario1,300.0));
+		leilao.propoe(new Lance(usuario2,400.0));
+		double resultadoEsperado1 = 400;
+		// executando a ação
+		avaliador.avalia(leilao);
+		// comparando a saída com o esperado
+		double resultadoObtido1 = avaliador.getMaiorLance();
+		assertEquals(resultadoEsperado1, resultadoObtido1, 0.0001);
+
+		}
 		
-  
+		@Test
+		public void testarMenorLancesOrdemCrescente() {
+
+		// cenário: 3 lances sem ordem
+		leilao.propoe(new Lance(usuario3,250.0));
+		leilao.propoe(new Lance(usuario1,300.0));
+		leilao.propoe(new Lance(usuario2,400.0));
+		double resultadoEsperado = 250;
+		// executando a ação
+		avaliador.avalia(leilao);
+		// comparando a saída com o esperado
+		double resultadoObtido = avaliador.getMenorLance();
+		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+
+		}
+		
+		@Test
+		public void testarMenorLancesSemOrdem() {
+		// cenário: 3 lances sem ordem
+		leilao.propoe(new Lance(usuario1,300.0));
+		leilao.propoe(new Lance(usuario2,400.0));
+		leilao.propoe(new Lance(usuario3,250.0));
+		double resultadoEsperado = 250;
+		// executando a ação
+		avaliador.avalia(leilao);
+		// comparando a saída com o esperado
+		double resultadoObtido = avaliador.getMenorLance();
+		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+
+		}
+		
+		@Test
+		public void testarMenorLancesUmLance() {
+		// cenário: 3 lances sem ordem
+		leilao.propoe(new Lance(usuario3,250.0));
+		double resultadoEsperado = 250;
+		// executando a ação
+		avaliador.avalia(leilao);
+		// comparando a saída com o esperado
+		double resultadoObtido = avaliador.getMenorLance();
+		assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+		}
+		
+		@Test
+		public void testarTresMaioresLancesOrdemCrescente() {
+		leilao.propoe(new Lance(usuario1, 100.0));
+		leilao.propoe(new Lance(usuario2, 200.0));
+		leilao.propoe(new Lance(usuario1, 300.0));
+		leilao.propoe(new Lance(usuario2, 400.0));
+		avaliador.avalia(leilao);
+		List<Lance> maiores = avaliador.getTresMaiores();
+		assertEquals(3, maiores.size());
+		assertEquals(400, maiores.get(0).getValor(), 0.00001);
+		assertEquals(300, maiores.get(1).getValor(), 0.00001);
+		assertEquals(200, maiores.get(2).getValor(), 0.00001);
+
+		}
+		
+		@Test
+		public void naoDeveAvaliarLeiloesSemNenhumLanceDado() {
+			try {
+				avaliador.avalia(leilao);
+			} catch (Exception e) {
+				// tudo ok;
+			}
+		}
+		
+		
 	}
 
-}
+
