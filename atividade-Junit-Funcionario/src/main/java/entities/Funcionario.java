334be099d2b2062abab95d5ad942877683f20231
Laiane Ribeiro <Laiane Ribeiro@DESKTOP-94BMI1G>
2022-10-20 22:20:07 -0300

    Atualizando classe Funcionário e Adicionando testes

--- atividade-Junit-Funcionario/src/main/java/entities/Funcionario.java
+++ atividade-Junit-Funcionario/src/main/java/entities/Funcionario.java
@@ -3,16 +3,23 @@ package entities;
 public class Funcionario {
 	public static final int VALORMAXIMO_DESPESAS = 1000;
 	public static final double VALORMAXIMO_HORASTRABALHADAS = 40.0;
-	//public static final int LIMITE_INFERIOR_VALORPORHORA = valorhora/4*100;
+	public static final double LIMITE_INFERIOR_VALORPORHORA = 48.48;
+	public static final double LIMITE_SUPERIOR_VALORPORHORA = 121.20;
 	private String nome;
 	private double horastrabalhadas;
 	private double valorhora;
 
 	
-	public Funcionario (String nome, double horastrabalhadas, double valorHora) {
+	public Funcionario (String nome, double horastrabalhadas, double valorhora) {
+			if (horastrabalhadas > VALORMAXIMO_HORASTRABALHADAS) {
+				throw new IllegalArgumentException("O Valor máximo de horas trabalhadas é 40");
+				}
+			if (valorhora < LIMITE_INFERIOR_VALORPORHORA || valorhora > LIMITE_SUPERIOR_VALORPORHORA) {
+				throw new IllegalArgumentException("O valor por hora precisa ser entre 4% e 10% do salário mínimo");
+			}
 		this.nome = nome;
 		this.horastrabalhadas = horastrabalhadas;
-		this.valorhora = valorHora;
+		this.valorhora = valorhora;
 	}
 
 
@@ -48,6 +55,13 @@ public class Funcionario {
 	// public boolean estaAprovado() {
 	//	return (valor < NOTA_MINIMA || frequencia < FREQUENCIA_MINIMA ? false : true);
 	//}
+	public double CalcularPagamento() {
+		double salario=(horastrabalhadas * valorhora *4 );
+		if (salario < 1212.00) {
+			throw new IllegalArgumentException("O Valor do salario est abaixo de um salario minimo");
+			}
+	    return (salario);
+	}
 
 
 }
--- atividade-Junit-Funcionario/src/test/java/entities/FuncionarioTeste.java
+++ atividade-Junit-Funcionario/src/test/java/entities/FuncionarioTeste.java
@@ -9,7 +9,7 @@ import entities.Funcionario;
       que corresponde a R$ 1.212,00. No funcionário terceirizado não considere o bônus nessa analise,ou seja, é a mesma verificação do funcionário   
     - Os funcionários podem trabalhar por no máximo 40 horas.
     -  O valor por hora precisa ser entre 4% e 10% do salário mínimo.
-    -  O valor das despesas adicionais não pode ultrapassar 1000,00.
+    -  opcional O valor das despesas adicionais não pode ultrapassar 1000,00.
  * @author Laiane Ribeiro 
  *
  */
@@ -18,16 +18,18 @@ public class FuncionarioTeste {
 	
 	//CT01
 	@Test
-	public void testarConstrutorFuncnionarioComDadosValidos(){
+	public void testarConstrutorFuncnionarioComDadosValidosesalariovalido(){
 		
 		//Cenário de teste
 	    String nomeValido = "Laiane Ribeiro";
 	    double horasTrabalhadasvalida = 35.0;
-	    double valorHoravalida = 35.0;
+	    double valorHoravalida = 50.0;
+	    double salarioEsperado = 7000;
 	 	Funcionario f;	
 		
 		//execução
 		f = new Funcionario(nomeValido,horasTrabalhadasvalida,valorHoravalida);
+		double salarioObtido = f.CalcularPagamento();
 		String nomeObtido = f.getNome();
 	    double horastrabalhadasObtidas = f.getHorastrabalhadas();
 	    double valorhorasObtidas = f.getValorhora();
@@ -36,6 +38,7 @@ public class FuncionarioTeste {
 	    Assertions.assertEquals(nomeValido, nomeObtido);
 	    Assertions.assertEquals(horasTrabalhadasvalida, horastrabalhadasObtidas);
 	    Assertions.assertEquals(valorHoravalida, valorhorasObtidas);
+	    Assertions.assertEquals(salarioEsperado, salarioObtido);
 	   
 	}
 	
