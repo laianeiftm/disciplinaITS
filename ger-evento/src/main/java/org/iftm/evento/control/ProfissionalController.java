d8b1f9b394abbdfa86bbfbc15f31f7721a5ac675
Laiane Ribeiro <Laiane Ribeiro@DESKTOP-94BMI1G>
2022-12-01 22:18:07 -0300

    Criação do CRUD e telas adicionais

--- ger-evento/src/main/java/org/iftm/evento/control/ProfissionalController.java
+++ ger-evento/src/main/java/org/iftm/evento/control/ProfissionalController.java
@@ -1,21 +1,74 @@
 package org.iftm.evento.control;
+import java.util.Optional;
 
+import org.iftm.evento.model.Profissional;
+import org.iftm.evento.model.repository.ProfissionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 // @RequestMapping("/profissional")
 
+
 public class ProfissionalController {
+	
+	@Autowired
+	ProfissionalRepository repositorio;
+	
 	@GetMapping("/sobre")
 	public String sobre() {
 		return "Sobre";
 		}
+	
+	@GetMapping("delete/{id}")
+	public String delete(@PathVariable int id) {  
+	  	Optional<Profissional> Profissional = repositorio.findById(id);
+	      if (Profissional.isPresent()) {
+	            		repositorio.delete(Profissional.get());
+	    	}        
+	        return "redirect:/home";
+	    }
+
+    @GetMapping("/form")
+    public String profissionalForm(Profissional profissional) {    	
+        return "addProfissionalForm";
+        
+    }
+    
+    @PostMapping("/add")
+    public String novo(Profissional profissional) {
+        repositorio.save(profissional);
+        return "redirect:/home";
+    }
+
+    @GetMapping("form/{id}")
+    public String updateForm(Model model, @PathVariable int id) {
+    	Optional<Profissional> profissional = repositorio.findById(id);
+    	System.out.println(profissional.get().getCodProfissional());
+    	if (profissional.isPresent()) {
+    		model.addAttribute("profissional", profissional.get());
+    		return "atualizaProfissionalForm";
+    	}else {
+    		return "redirect:/home";
+    	}
+    }
+    
+    @PostMapping("update/{id}")
+    public String alterarProduto(Profissional profissional, @PathVariable int id) {
+    	System.out.println(profissional.getCodProfissional());
+       repositorio.save(profissional);
+        return "redirect:/home";
+    }
+
+    
 
 
-	public ProfissionalController() {
-		// TODO Auto-generated constructor stub
-	}
 
+	
 }
--- ger-evento/src/main/java/org/iftm/evento/control/homeController.java
+++ ger-evento/src/main/java/org/iftm/evento/control/homeController.java
@@ -1,17 +1,38 @@
 package org.iftm.evento.control;
 
+import java.util.List;
+
+import org.iftm.evento.model.Profissional;
+import org.iftm.evento.model.repository.ProfissionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@Autowired
-private ProfissionalRepository profRepository;
+@Controller
+public class homeController {
 
-@GetMapping("/home")
-public String home(Model model) {
-	List<Veterinario> veterinarios;
-	veterinarios = vetRepository.findAll();
-	model.addAttribute("veterinarios", veterinarios);
-	return "home";
+	@Autowired
+	ProfissionalRepository repositorio;
+
+	@GetMapping("/home")
+	public String home(Model model) {
+		List<Profissional> profissionais;
+		profissionais = repositorio.findAll();
+		model.addAttribute("profissionais", profissionais);
+		return "home";
+	}
+	
+  
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
-
-
